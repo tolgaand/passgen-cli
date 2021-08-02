03f247b2f2f871e8f89d455ce6387dad44d130d7
Tolga <tolgaand@yandex.com>
2021-08-02 19:44:35 +0300

    Create the `index.js`

---
+++ index.js
@@ -0,0 +1,32 @@
+const program = require("commander");
+const chalk = require("chalk");
+const clipboardy = require("clipboardy");
+const createPassword = require("./utils/createPassword");
+const savePassword = require("./utils/savePassword");
+
+program.version("1.0.0").description("Simple Password Generator");
+
+program
+  .option("-l, --length <number>", "length of password", "8")
+  .option("-s, --save", "save password to passwords.txt")
+  .option("-nn, --no-numbers", "remove numbers")
+  .option("-ns, --no-symbols", "remove symbols")
+  .parse();
+
+const { length, save, numbers, symbols } = program.opts();
+
+const generatedPassword = createPassword(length, save, numbers, symbols);
+
+clipboardy.writeSync(generatedPassword);
+
+console.log(chalk.blue("Generated Password: ") + chalk.bold(generatedPassword));
+console.log(chalk.yellow("Password copied to clipboard"));
+
+if (save) savePassword(generatedPassword);
+
+// program
+//   .command("generate")
+//   .action(() => {
+//     console.log("generated");
+//   })
+//   .parse();
